b75d6ceb54ee1a28e4a7146e8c2b5e0feefb722a
unknown <sanjidaema81@gmail.com>
2024-04-08 14:23:10 +0600

    created routes and navbar of a recidential project

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0i0TcklAAS0SgfCfnDgXv5h8emDoQ8oU",
+  authDomain: "m53-assignment-project.firebaseapp.com",
+  projectId: "m53-assignment-project",
+  storageBucket: "m53-assignment-project.appspot.com",
+  messagingSenderId: "604135774917",
+  appId: "1:604135774917:web:689d407f474aa58eb36eac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">log in page</h2>
+        </div>
+    );
+};
+
+export default Login;
