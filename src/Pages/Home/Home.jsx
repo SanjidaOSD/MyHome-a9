437e8c55d7aad4eda712065531d244b635de0850
unknown <sanjidaema81@gmail.com>
2024-04-10 12:47:01 +0600

    created animated card

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,14 +1,66 @@
+import { useEffect, useState } from "react";
 import Slider from "../../Components/Slider";
+import { IoHome } from "react-icons/io5";
+import { FaLocationDot } from "react-icons/fa6";
+// AOS
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
+
+
 
 
 
 const Home = () => {
+    const [cards, setCards] = useState([]);
+
+    // const {state_title, image, segment_name, status} = cards;
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setCards(data))
+    }, []);
+
     return (
         <div>
-            
-            <h3 className="text-3xl font-poppins">This is home</h3>
             <Slider></Slider>
-            
+
+            <div className="mt-24">
+                <div>
+                    <h2 className=" flex items-center md:text-5xl text-3xl justify-center font-bold text-center mb-12"><IoHome></IoHome>
+                        Latest Listings<IoHome></IoHome></h2>
+
+                </div>
+                <div
+                    className="md:grid md:grid-cols-2 lg:grid-cols-3 justify-center items-center gap-16">
+
+                    {
+                        cards.map(card => <div key={card.id}>
+
+                            <div data-aos="fade-up"
+                                data-aos-duration="2000" className="card w-96 bg-base-100 shadow-xl">
+                                <figure><img src={card.image} alt="Shoes" /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title"><IoHome className="w-8 h-8"></IoHome>
+                                        {card.state_title}</h2>
+                                    <p className="font-bold">{card.segment_name}</p>
+                                    <p className="flex"><FaLocationDot />
+                                        {card.location}</p>
+
+                                    <div className="card-actions">
+                                        <button className="btn bg-slate-600 w-full text-white hover:text-black">Show Details</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>)
+                    }
+
+                </div>
+
+
+            </div>
+
         </div>
     );
 };
